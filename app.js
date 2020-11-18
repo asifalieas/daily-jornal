1805ddb8a474ba70919d253b7978953f694245eb
asifali2705 <asifalieas@gmail.com>
2020-11-18 11:49:27 +0530

    added procfile

--- app.js
+++ app.js
@@ -65,6 +65,10 @@ app.post('/compose', function (req, res) {
 
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
